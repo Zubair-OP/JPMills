894da8a50e96235563ead665518b353c4273706e
ZUBAIR-OP <zubairlegend18@gmail.com>
2025-09-05 22:40:22 +0500

    new updates

--- src/Components/customers/Customer.jsx
+++ src/Components/customers/Customer.jsx
@@ -7,11 +7,11 @@ function Customer() {
         <h2 className='lg:font-[four] lg:text-[2.5vw] text-[9vw] font-[one] uppercase text-center py-10'>Customers</h2>
         <div className='w-full flex flex-col lg:flex-row gap-10 items-center justify-evenly'>
           <img className='lg:w-25 w-[40vw] h-10' src="images/Banana-Republic.png" alt="" />
-          <img className='lg:w-25 w-[40vw] h-10' src="public\images\next.png" alt="" />
-          <img className='lg:w-25 w-[40vw] h-10' src="public\images\zara-1.png" alt="" />
-          <img className='lg:w-25 w-[40vw] h-10' src="public\images\levi-logo-1.png" alt="" />
-          <img className='lg:w-25 w-[40vw] h-10' src="public\images\lee.png" alt="" />
-          <img className='lg:w-25 w-[40vw] h-10' src="public\images\Khaadi-logo.png" alt="" />
+          <img className='lg:w-25 w-[40vw] h-10' src="images/next.png" alt="" />
+          <img className='lg:w-25 w-[40vw] h-10' src="images/zara-1.png" alt="" />
+          <img className='lg:w-25 w-[40vw] h-10' src="images/levi-logo-1.png" alt="" />
+          <img className='lg:w-25 w-[40vw] h-10' src="images/lee.png" alt="" />
+          <img className='lg:w-25 w-[40vw] h-10' src="images/Khaadi-logo.png" alt="" />
         </div>
       </div>
     </div>
